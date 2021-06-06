5f643b40f089d4481f56dc6228af5144a87a6dd4
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-06 09:41:22 -0500

    Initial config with connection.js commit

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// Require for environment variables
+require("dotenv").config();
+
+// Local variables
+const Sequelize = require("sequelize");
+const jawsDbUrl = process.env.JAWSDB_URL;
+let sequelize;
+
+// Check if environment has JawsDB available
+if (jawsDbUrl) {
+
+    // Assigns variable to use JawsDB
+    sequelize = new Sequelize(jawsDbUrl);
+
+} else {
+
+    // Assigns variable to use local MySQL
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
