445fce591afab03c14864932f75dafe11aa5eb12
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-07 10:43:35 -0500

    Added comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
+// Local variables to call on packages
 const router = require("express").Router();
+
+// Local variables to call another JS file
 const mainRoute = require("./mainRoute");
 
+// Enable router to use path
 router.use("/", mainRoute);
 
+// Export
 module.exports = router;
