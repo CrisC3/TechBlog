d47844fefcef8b3a5ed8a067af1e183eccf4ac52
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-07 11:24:22 -0500

    Added the routes to the controller, and have express use it

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Local variables to call on packages
 const express = require("express");
 const sequelize = require("./config/connection");
+const routes = require('./controllers');
 
 // Local declare variables
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set to use the [Routes]
+app.use(routes);
 
 // Call to sequelize and start application
 sequelize.sync({ force: false})
