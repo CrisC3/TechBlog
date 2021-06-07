1af3dbb5c855162d483d462dfd5dea8b6b85ec6b
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-07 11:23:16 -0500

    Added init with PK

--- models/User.js
+++ models/User.js
@@ -5,4 +5,15 @@ const sequelize = require('../config/connection');
 
 class User extends Model {}
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    }
+);
+
 module.exports = User;
