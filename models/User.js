72a00105b14c6cb0a3ba7840c01de4f0e5c2c7b4
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-07 11:19:07 -0500

    Initial commit

--- models/User.js
+++ models/User.js
@@ -1,3 +1,8 @@
+// Local variables to call on packages
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
 
 module.exports = User;
