c9590916aabcbd3ac2d14b85bf77f9143a3bb163
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 18:55:03 -0500

    Updated blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,1 +1,29 @@
-// const router = require('express').Router();
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../../models');
+
+// GET /api/blogs
+router.get('/', async (req, res) => {
+    
+    try {
+
+        const blogData = await Blog.findAll();
+
+        
+    } catch (error) {
+        
+    }
+});
+
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
