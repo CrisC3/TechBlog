988882eae10b74ba9fdb2884f18704ff5a14d4a3
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 12:43:05 -0500

    Added controllers/mainRoute.js GET method

--- controllers/mainRoute.js
+++ controllers/mainRoute.js
@@ -2,4 +2,25 @@
 const router = require('express').Router();
 const {User, Blog, Comment} = require("../models");
 
+router.get("/", async (req, res) => {
+    
+    try {
+        
+        // Get all blogs JOIN with user table
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
+
+        res.status(200).json(blogData);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
