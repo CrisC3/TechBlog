a9e18b53aef6e3b89423b49c9c3757fe5dd4541d
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 08:13:04 -0500

    Updated variable to no longer point directly to the User model

--- controllers/mainRoute.js
+++ controllers/mainRoute.js
@@ -1,5 +1,5 @@
 // Local variables to call on packages
 const router = require('express').Router();
-const {User} = require("../models/User");
+const {User} = require("../models");
 
 module.exports = router;
