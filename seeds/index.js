c2649bf228dd695e89889e7b03a62bf2d4e54655
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 09:52:21 -0500

    Updated to sync user data into MySQL

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,19 @@
 const sequelize = require("../config/connection");
 const { User, Blog, Comment } = require("../models");
 
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+      });
+
+      process.exit(0);
+
+};
+
+seedDatabase();
