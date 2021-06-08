58ccdabed0198ad58744339229f9317452d8c36f
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 15:40:50 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 // Local declare variables
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Require for POST method
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use to load static files from the local public folder
+app.use(express.static("public"));
 
 // Set to use the [Routes]
 app.use(routes);
