c047c8899bf9e178820cba1727e5bf1d0b33e71e
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-06-08 13:06:37 -0500

    Updated User to blog relationship

--- models/index.js
+++ models/index.js
@@ -12,6 +12,10 @@ User.hasMany(Comment, {
     onDelete: 'CASCADE'
 });
 
+Blog.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
 Blog.hasMany(Comment, {
     foreignKey: 'blog_id',
     onDelete: 'CASCADE'
